f1d7cb453ae2cba399f24fccadcb7074a24950b8
RajenderDandyal <rajender0184@gmail.com>
2019-04-07 16:30:59 +0530

    added global error handling

--- src/app.js
+++ src/app.js
@@ -9,4 +9,36 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+// error handling
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.message = 'Invalid route';
+    error.status = 404;
+    next(error);
+});
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.json({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
+// log errors to console
+function logErrors (err, req, res, next) {
+    console.error(err.stack)
+    next(err)
+}
+// error handling for xhr request
+function clientErrorHandler (err, req, res, next) {
+    if (req.xhr) {
+        res.status(500).send({ error: err.message })
+    } else {
+        next(err)
+    }
+}
+
 module.exports = app
